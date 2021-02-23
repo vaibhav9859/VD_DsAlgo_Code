6bdbfd14a3eaa02891a9b9e2c6160cf0329072b6
Vaibhav Deshmukh <36755262+vaibhav9859@users.noreply.github.com>
2021-02-23 22:51:03 +0530

    CreatingBinaryTree_UsingUserInput_Inorder&preorderTraversal

---
+++ General/Tree/CreatingBinaryTree_UsingUserInput_Inorder&preorderTraversal.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+template <typename T>
+class BinaryTreeNode{
+public:
+    T data;
+    BinaryTreeNode<T>* left;
+    BinaryTreeNode<T>* right;
+
+    BinaryTreeNode(T data){
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+
+    ~BinaryTreeNode(){
+        delete left;
+        delete right;
+    }
+};
+
+void printNodes(BinaryTreeNode<int>* root){
+    if(!root) return;
+    cout<<root->data<<" ";
+    printNodes(root->left);
+    printNodes(root->right);
+}
+
+void printInorder(BinaryTreeNode<int>* root){
+    if(!root) return;
+    printInorder(root->left);
+    cout<<root->data<<" ";
+    printInorder(root->right);
+}
+
+BinaryTreeNode<int> * takeInput(){
+    cout<< "Put data "<<endl;
+    int data;
+    cin>>data; if(data ==-1) return NULL;
+    BinaryTreeNode<int> * node = new BinaryTreeNode<int>(data);
+    node->left = takeInput();
+    node->right = takeInput();
+    return node;
+}
+
+int main(){
+    BinaryTreeNode<int>* root = takeInput();
+    printNodes(root);
+    cout<<endl;
+    printInorder(root);
+    
+    return 0;
+}
